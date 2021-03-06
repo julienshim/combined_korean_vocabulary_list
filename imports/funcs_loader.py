312fdbae4ce644f676d7631ee4bdd1961b92b364
julienshim <25723428+julienshim@users.noreply.github.com>
2021-03-05 21:15:29 -0800

    close

--- imports/funcs_loader.py
+++ imports/funcs_loader.py
@@ -4,4 +4,4 @@ def load_body(address):
     with open(address) as tsv_file:
         tsv_file_headers = tsv_file.readline() # cuts the headers
         return tsv_file.readlines() # cuts the content
-        go.close()
+        tsv_file.close()
