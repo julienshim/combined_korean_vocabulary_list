b7db03554155bbe9ffa1d3d50f0ced611bfd375d
julienshim <25723428+julienshim@users.noreply.github.com>
2025-03-01 03:08:31 -0800

    Remove deprecated files and old code after major refactoring
    Deleted outdated utility functions, unused test files, and other obsolete code as part of the recent codebase overhaul.

--- imports/funcs_loader.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def load_body(address):
-    with open(address) as tsv_file:
-        tsv_file_headers = tsv_file.readline() # cuts the headers
-        return tsv_file.readlines() # cuts the content
-        tsv_file.close()
--- imports/lists.py
+++
@@ -1,2 +0,0 @@
-khen_targets = ('ko', 'en', 'ko/en', 'ko/num', 'han/en', 'ko/han','han/num', 'ko/han/num')
-combined_headers = ("frequency", "korean", "pos", "hanja", "hint", "go_level", "topik_level")
